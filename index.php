14a0d9c36ffdf97431d2cf4804df194db6269714
strangelights <jbrookshire7@gmail.com>
2019-03-04 18:56:45 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// Hi 
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Codebuilt Portfolio</title>
+  </head>
+  <body>
+    <h1>Josh Brookshire</h1>
+    <h2>Web Developer</h2>
+    <title>My Work</title>
+    <a href="https://github.com/strangelights/slack_dark_theme">Slack Dark Theme</a>
+    <a href="https://asta.rsglab.com/projects/ScriptChecker/">ScripChecker</a>
+    <a href="https://git.rsglab.com/support-org/scriptchecker">ScriptChecker - Private Repo</a>
+    <a href="https://git.rsglab.com/support-org/sev1">Sev1 - Private Repo</a>
+    <a href="#">Chronos Time Tracker - Coming Soon</a>
+  </body>
+</html>
+
