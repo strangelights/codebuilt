9cbb513360166985174cc27b317056b2c5921458
strangelights <jbrookshire7@gmail.com>
2019-03-04 20:23:58 -0500

    Fix link display

--- index.php
+++ index.php
@@ -14,9 +14,13 @@
     <h2>Web Developer</h2>
     <title>My Work</title>
     <a href="https://github.com/strangelights/slack_dark_theme">Slack Dark Theme</a>
-    <a href="https://asta.rsglab.com/projects/ScriptChecker/">ScripChecker</a>
+    <br>
+    <a href="https://asta.rsglab.com/projects/ScriptChecker/">ScriptChecker</a>
+    <br>
     <a href="https://git.rsglab.com/support-org/scriptchecker">ScriptChecker - Private Repo</a>
+    <br>
     <a href="https://git.rsglab.com/support-org/sev1">Sev1 - Private Repo</a>
+    <br>
     <a href="#">Chronos Time Tracker - Coming Soon</a>
   </body>
 </html>
