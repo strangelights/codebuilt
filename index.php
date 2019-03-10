04c5edc77cb8ab833c3626d4b08dc2c20a2c5cf3
strangelights <jbrookshire7@gmail.com>
2019-03-10 17:07:57 -0400

    Add styles

--- index.php
+++ index.php
@@ -8,11 +8,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Codebuilt Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-81785500-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-81785500-3');
+    </script>
   </head>
   <body>
     <h1>Josh Brookshire</h1>
     <h2>Web Developer</h2>
-    <title>My Work</title>
     <a href="https://github.com/strangelights/slack_dark_theme">Slack Dark Theme</a>
     <br>
     <a href="https://asta.rsglab.com/projects/ScriptChecker/">ScriptChecker</a>
@@ -21,8 +31,7 @@
     <br>
     <a href="https://git.rsglab.com/support-org/sev1">Sev1 - Private Repo</a>
     <br>
-    <a href="#">Chronos Time Tracker - Coming Soon</a>
-    <p>heroku test</p>
+    <a href="#">Untiled Time Tracker App</a>
   </body>
 </html>
 
