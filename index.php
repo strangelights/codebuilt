42a3e1318cfc291e224c117f2a8efea6decde17c
strangelights <jbrookshire7@gmail.com>
2019-03-10 18:54:41 -0400

    Add mail icon

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <title>Codebuilt Portfolio</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-81785500-3"></script>
     <script>
@@ -42,7 +43,7 @@
       <br>
       <a href="#">Time Tracker App</a>
     </div>
-    <footer>contact: <a href="mailto:josh@codebuilt.dev">josh@codebuilt.dev</a></footer>
+    <footer><i class="fas fa-envelope"></i><a href="mailto:josh@codebuilt.dev">josh@codebuilt.dev</a></footer>
   </body>
 </html>
 
