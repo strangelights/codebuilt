113dc15f8ba4cd83ecf2d71f0ac0589a04bf65aa
strangelights <jbrookshire7@gmail.com>
2020-03-23 08:54:58 -0400

    Update styles

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <title>Codebuilt Portfolio</title>
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,800,900" rel="stylesheet">
     <link rel="alternate stylesheet" type="text/css" href="darktheme.css" title="darktheme">
     <script src="https://kit.fontawesome.com/96aa305cf2.js" crossorigin="anonymous"></script>
 
@@ -65,55 +65,55 @@
     <h2 id="languages">HTML CSS JS PHP MYSQL</h2>
     </div>
     <div id="links">
-        <span class="project-type">Mailchimp:</span>
+        <span class="project-type">Mailchimp Projects</span>
         <br>
-        <a href="https://scriptchecker.platform.rsglab.com/" target="_blank">ScriptChecker</a>
+        <a class="project-title" href="https://scriptchecker.platform.rsglab.com/" target="_blank">ScriptChecker</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/support-org/scriptchecker" target="_blank">Repo</a>
-        <span class="contribution"> - Author, Maintainer</span>
+        <a class="project-repo" href="https://git.rsglab.com/support-org/scriptchecker" target="_blank">Repo</a>
+        <span class="contribution">Author, Maintainer</span>
         <br>
-        <a href="https://dnscheck.platform.rsglab.com/" target="_blank">DNSchecker</a>
+        <a class="project-title" href="https://dnscheck.platform.rsglab.com/" target="_blank">DNSchecker</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/josh-brookshire/DNScheck" target="_blank">Repo</a>
-        <span class="contribution"> - Contributor, Maintainer</span>
+        <a class="project-repo" href="https://git.rsglab.com/josh-brookshire/DNScheck" target="_blank">Repo</a>
+        <span class="contribution">Contributor, Maintainer</span>
         <br>
-        <a href="https://sirt.platform.rsglab.com/" target="_blank">Support Incident Response Tool</a>
+        <a class="project-title" href="https://sirt.platform.rsglab.com/" target="_blank">Incident Response Tool</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/josh-brookshire/sev1-pando" target="_blank">Repo</a>
-        <span class="contribution"> - Contributor, Maintainer</span>
+        <a class="project-repo" href="https://git.rsglab.com/josh-brookshire/sev1-pando" target="_blank">Repo</a>
+        <span class="contribution">Contributor, Maintainer</span>
         <br>
-        <a href="http://star-search.platform.rsglab.com/" target="_blank">STAR Search</a>
+        <a class="project-title" href="http://star-search.platform.rsglab.com/" target="_blank">STAR Search</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/josh-brookshire/star-search" target="_blank">Repo</a>
-        <span class="contribution"> - Contributor</span>
+        <a class="project-repo" href="https://git.rsglab.com/josh-brookshire/star-search" target="_blank">Repo</a>
+        <span class="contribution">Contributor</span>
         <br>
-        <a href="http://echo-comm.platform.rsglab.com/" target="_blank">EchoComm</a>
+        <a class="project-title" href="http://echo-comm.platform.rsglab.com/" target="_blank">EchoComm</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/josh-brookshire/echo-comm" target="_blank">Repo</a>
-        <span class="contribution"> - Contributor</span>
+        <a class="project-repo" href="https://git.rsglab.com/josh-brookshire/echo-comm" target="_blank">Repo</a>
+        <span class="contribution">Contributor</span>
         <br>
-        <a href="https://git.rsglab.com/rsg/supportbot/blob/master/README.md" target="_blank">SupportBot</a>
+        <a class="project-title" href="https://git.rsglab.com/rsg/supportbot/blob/master/README.md" target="_blank">SupportBot</a>
         <span> - </span>
-        <a href="https://git.rsglab.com/josh-brookshire/supportbot" target="_blank">Repo</a>
-        <span class="contribution"> - Contributor</span>
+        <a class="project-repo" href="https://git.rsglab.com/josh-brookshire/supportbot" target="_blank">Repo</a>
+        <span class="contribution">Contributor</span>
         <br>
-        <span class="project-type">Personal:</span>
+        <span class="project-type">Personal Projects</span>
         <br>
-        <a href="https://random-quote-generator-website.herokuapp.com/" target="_blank">Random Quote Generator</a>
+        <a class="project-title" href="https://random-quote-generator-website.herokuapp.com/" target="_blank">Random Quote Generator</a>
         <span> - </span>
-        <a href="https://github.com/strangelights/techdegree-project-1" target="_blank">Repo</a>
+        <a class="project-repo" href="https://github.com/strangelights/techdegree-project-1" target="_blank">Repo</a>
         <br>
-        <a href="https://strangelights.github.io/highlighter/highlighter.html" target="_blank">Highlighter Clipboard Utility</a>
+        <a class="project-title" href="https://strangelights.github.io/highlighter/highlighter.html" target="_blank">Highlighter Clipboard Utility</a>
         <span> - </span>
-        <a href="https://github.com/strangelights/highlighter" target="_blank">Repo</a>
+        <a class="project-repo" href="https://github.com/strangelights/highlighter" target="_blank">Repo</a>
         <br>
-        <a href="https://www.codebuilt.dev/" target="_blank">Development Portfolio</a>
+        <a class="project-title" href="https://www.codebuilt.dev/" target="_blank">Development Portfolio</a>
         <span> - </span>
-        <a href="https://github.com/strangelights/codebuilt" target="_blank">Repo</a>
+        <a class="project-repo" href="https://github.com/strangelights/codebuilt" target="_blank">Repo</a>
         <br>
-        <a href="https://raw.githubusercontent.com/strangelights/slack_dark_theme/master/slack_dark_theme.png" target="_blank">Slack Dark Theme</a>
+        <a class="project-title" href="https://raw.githubusercontent.com/strangelights/slack_dark_theme/master/slack_dark_theme.png" target="_blank">Slack Dark Theme</a>
         <span> - </span>
-        <a href="https://github.com/strangelights/slack_dark_theme" target="_blank">Repo</a>
+        <a class="project-repo" href="https://github.com/strangelights/slack_dark_theme" target="_blank">Repo</a>
     </div>
     <footer><i id="envelope"class="fas fa-envelope"></i><a href="mailto:josh@codebuilt.dev">josh@codebuilt.dev</a></footer>
 </body>
